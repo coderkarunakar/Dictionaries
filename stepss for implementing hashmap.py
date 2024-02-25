c96f4e0941b9f56cfb85354211a80dcb8e06c339
Karunakar chembeti <114292518+coderkarunakar@users.noreply.github.com>
2024-02-25 16:12:12 +0530

    steps for implementing hashmap

---
+++ stepss for implementing hashmap.py
@@ -0,0 +1,10 @@
+#Steps for implementing hashmap
+#what we need:
+#Class Map:1.Buckets,Bucket Size,Count
+
+#Class mapnode:key,value,next
+
+#hashmap will involve 3 most important function
+# 1.insert(key,value)
+#2.remvoe(key)
+#3.search(key)
