07ae8bdd3de3d69a530eaeea71495d67123c343e
Karunakar chembeti <114292518+coderkarunakar@users.noreply.github.com>
2024-02-28 09:47:03 +0530

    Time complexity for map function

--- search and remove from map.py
+++ search and remove from map.py
@@ -70,3 +70,15 @@ print(m.getValue('Rohan'))
 print(m.getValue('Parikh'))
 m.remove('Rohan')
 print(m.getValue('Rohan'))
+
+
+#Time complexity
+# insert (key,value)
+#first you get a key then you get a hash code to that,then you get an index corresponding to that ,you see through this linked list if that element is already present or not,if present then replace
+#time which is taking is calculating the hash code  i.e O(l) ,and iterating on linked list O(n)
+#so i say O(l) is very much lesser than O(n) because max lenght of the string will be 4 or 5 so o(l) is constant no
+#so o(n) need to be improved
+
+#if there are b boxes then n/b each entrie takes place,and this n/b should remain less thatn 0.7 (this factor is called as load factor) n->no of entries ,b->bucket size
+
+#Rehashing: it initializes a new array of double size and what ever the previous element was,will try to store it in the new array whose size is the double of the previous size,increase the bucket size maintain n/b < 0.7
