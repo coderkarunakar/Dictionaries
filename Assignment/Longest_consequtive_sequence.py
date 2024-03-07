93fec44d1795dd8bb1e7da779642abc37e24e2cf
Karunakar chembeti <114292518+coderkarunakar@users.noreply.github.com>
2024-03-07 08:46:27 +0530

    dry run

--- Assignment/Longest_consequtive_sequence.py
+++ Assignment/Longest_consequtive_sequence.py
@@ -20,23 +20,28 @@
 # 
 # 
 def longest_consecutive_sequence(nums):
+    #checks if nums is empty
     if not nums:
         return 0
-    
+        #converts nums list to a set for faster lookup
     num_set = set(nums)
+    #initiate the max lenght of consecutive sequence
     max_length = 0
-    
+    #iterate through each num in set
     for num in num_set:
+        #checks if prev num -1 is not in the set,it means num is the start of a new sequence
         if num - 1 not in num_set:
             current_num = num
             current_length = 1
-            
+     # Iterate through consecutive numbers from current_num and increment current_length
+
             while current_num + 1 in num_set:
                 current_num += 1
                 current_length += 1
-                
+     # Update max_length if current_length is greater
+
             max_length = max(max_length, current_length)
-    
+        # Return the maximum length of consecutive sequence
     return max_length
 
 # Example usage:
